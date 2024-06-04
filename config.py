ae7fc2081509d0bd9e4621f7cf7a57813829a605
BrhamaKumar <146445615+BrhamaKumar@users.noreply.github.com>
2024-06-04 21:11:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import environ  
   
 class Config:
-     API_ID = int(environ.get("API_ID", )) 
-     API_HASH = environ.get("API_HASH", "") 
-     BOT_TOKEN = environ.get("BOT_TOKEN", "")  
+     API_ID = int(environ.get("API_ID", 10000844)) 
+     API_HASH = environ.get("API_HASH", "776f257fc1d1f8aa4aea9dd35d10a45b") 
+     BOT_TOKEN = environ.get("BOT_TOKEN", "6998167875:AAEr3DxL2t5zA37GuhRDZ1r6xrBwSIcQS5E")  
      BOT_SESSION = environ.get("BOT_SESSION", "bot")  
-     DATABASE_URI = environ.get("DATABASE", "") 
+     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://fiona171593:tbGMvepmKQ8YNfJy@cluster0.5ccbrkf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") 
      DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0") 
-     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()] 
+     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '5019308664').split()] 
   
 class temp(object): 
      lock = {} 
